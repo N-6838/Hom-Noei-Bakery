5db7449b2a308fe7017dca7ec9d2266dbde3261d
N-6838 <suwankonkamon@gmail.com>
2026-01-12 13:15:23 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -9,35 +9,63 @@ function updateCartDisplay() {
 }
 updateCartDisplay();
 
-// ฟังก์ชันเพิ่มสินค้า
-function addToCart() {
-    let count = parseInt(localStorage.getItem("cartCount")) || 0;
-    count += 1;
-    localStorage.setItem("cartCount", count); 
-
-    updateCartCount();  // อัปเดตบนหน้าเดิมด้วย
-}
-
-// ฟังก์ชันล้างตะกร้าในหน้า cart.html
-function clearCart() {
-  count = 0;
-  localStorage.setItem("cartCount", 0);
-  updateCartDisplay();
-
-}
-
-// ฟังก์ชันอัปเดตตัวเลขตะกร้า
-function updateCartCount() {
-    let count = localStorage.getItem("cartCount") || 0;
-    document.getElementById("cart-count").innerText = count; 
-}
-
-// เรียกทุกครั้งที่โหลดหน้า
-updateCartCount();
-
 function updateCartPage() {
-    let count = localStorage.getItem("cartCount") || 0;
-    document.getElementById("total").innerText = count + " ชิ้น";
+  let count = localStorage.getItem("cartCount") || 0;
+  document.getElementById("total").innerText = count + " ชิ้น";
 }
 
 updateCartPage();
+
+// ดึงตะกร้า
+function getCart() {
+  return JSON.parse(localStorage.getItem("cart")) || [];
+}
+
+// บันทึกตะกร้า
+function saveCart(cart) {
+  localStorage.setItem("cart", JSON.stringify(cart));
+  localStorage.setItem("cartCount", cart.reduce((s, i) => s + i.qty, 0));
+
+
+// อัปเดตจำนวนในไอคอนตะกร้า
+  const count = cart.reduce((sum, item) => sum + item.qty, 0);
+  localStorage.setItem("cartCount", count);
+}
+
+function clearCart() {
+  localStorage.removeItem("cart");
+  localStorage.removeItem("cartCount");
+  location.reload();
+}
+
+// เพิ่มสินค้า
+function addToCart(name, price, image) {
+  let cart = getCart();
+
+  let found = cart.find(item => item.name === name);
+  if (found) {
+    found.qty += 1;
+  } else {
+    cart.push({
+      name: name,
+      price: price,
+      image: image,
+      qty: 1
+    });
+  }
+
+   saveCart(cart);
+  updateCartCount();
+}
+
+function updateCartCount() {
+  document.getElementById("cart-count").innerText =
+    localStorage.getItem("cartCount") || 0;
+}
+
+function clearCart() {
+  localStorage.clear();
+  location.reload();
+}
+
+updateCartCount();
